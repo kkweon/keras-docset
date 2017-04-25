51a04525b49a3dcd3a32c01468052ea68b669a67
KYUNG MO KWEON <kkweon@gmail.com>
2017-04-24 22:35:47 -0700

    add: adjust padding

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+cp theme*.css keras.io/css/
+find '.' -type f -name '*.1.html' -exec rm {} \;
 cp dashing.json keras.io/
 cd keras.io && dashing build
+cd ..
--- run_preprocess.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp theme.css keras.io/css/
-find '.' -type f -name '*.1.html' -exec rm {} \;
